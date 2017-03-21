1c414323a55452300fa34219f2a7375a9bcae305
Abhinav Tripathi <abtripathi@expedia.com>
2017-03-20 22:50:12 -0700

    Use firebase for login instead of phone for now

--- app/src/main/java/com/abhirishi/personal/collabarm/FirstActivity.java
+++ app/src/main/java/com/abhirishi/personal/collabarm/FirstActivity.java
@@ -3,17 +3,54 @@ package com.abhirishi.personal.collabarm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FirstActivity extends AppCompatActivity {
+
+	private static final int SIGN_IN_REQUEST_CODE = 100;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_first);
-		if (!User.isLoggedIn()) {
-			Intent intent = new Intent(this, PhoneAuthActivity.class);
-			startActivity(intent);
+
+		if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+			tryLogin();
 		}
+		else {
+			onAlreadyLoggedIn();
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+		Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == SIGN_IN_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				onAlreadyLoggedIn();
+			}
+			else {
+				tryLogin();
+			}
+		}
+	}
+	private void onAlreadyLoggedIn() {
+		Intent intent = new Intent(this, CollabarmActivity.class);
+		startActivity(intent);
 		finish();
 	}
+
+	private void tryLogin() {
+		startActivityForResult(
+			AuthUI.getInstance()
+				.createSignInIntentBuilder()
+				.setTheme(R.style.AppTheme)
+				.build(),
+			SIGN_IN_REQUEST_CODE
+		);
+	}
+
 }
