fe062f20736d1e42e2daabc3f144141dc8cac605
Abhinav Tripathi <abtripathi@expedia.com>
2017-03-21 23:41:50 -0700

    Ask for contacts permission

--- app/src/main/java/com/abhirishi/personal/collabarm/FirstActivity.java
+++ app/src/main/java/com/abhirishi/personal/collabarm/FirstActivity.java
@@ -1,20 +1,47 @@
 package com.abhirishi.personal.collabarm;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 
+import static android.Manifest.permission.READ_CONTACTS;
+
 public class FirstActivity extends AppCompatActivity {
 
 	private static final int SIGN_IN_REQUEST_CODE = 100;
+	private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 99;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_first);
+		checkPermissionsForReadingContacts();
+	}
 
+	private void checkPermissionsForReadingContacts() {
+		if (ContextCompat.checkSelfPermission(this, READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this, READ_CONTACTS)) {
+
+			}
+			else {
+				ActivityCompat.requestPermissions(this,
+					new String[] { READ_CONTACTS },
+					MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+			}
+		}
+		else {
+			authenticateUser();
+		}
+
+	}
+
+	private void authenticateUser() {
 		if (FirebaseAuth.getInstance().getCurrentUser() == null) {
 			tryLogin();
 		}
@@ -37,6 +64,7 @@ public class FirstActivity extends AppCompatActivity {
 			}
 		}
 	}
+
 	private void onAlreadyLoggedIn() {
 		Intent intent = new Intent(this, CollabarmActivity.class);
 		startActivity(intent);
@@ -51,6 +79,24 @@ public class FirstActivity extends AppCompatActivity {
 				.build(),
 			SIGN_IN_REQUEST_CODE
 		);
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode,
+		String permissions[], int[] grantResults) {
+		switch (requestCode) {
+		case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+			if (grantResults.length > 0
+				&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				authenticateUser();
+			}
+			else {
+
+			}
+			return;
+		}
+
+		}
 	}
 
 }
