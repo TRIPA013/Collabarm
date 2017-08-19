af3885df514b538a713dcd23d303bbe46e6e5294
Abhinav Tripathi <abtripathi@expedia.com>
2017-08-19 16:12:53 -0700

    Add ui for friend

--- app/src/main/java/com/abhirishi/personal/collabarm/FriendActivity.java
+++ app/src/main/java/com/abhirishi/personal/collabarm/FriendActivity.java
@@ -2,10 +2,16 @@ package com.abhirishi.personal.collabarm;
 
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import com.abhirishi.personal.collabarm.models.Friend;
 
@@ -13,6 +19,25 @@ public class FriendActivity extends AppCompatActivity {
 
 	private Friend friend;
 
+	private TextView mTextMessage;
+
+	private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+		= new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+		@Override
+		public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+			switch (item.getItemId()) {
+			case R.id.navigation_home:
+				mTextMessage.setText(R.string.title_activity_alarm);
+				return true;
+			case R.id.navigation_dashboard:
+				mTextMessage.setText(R.string.title_activity_alarm);
+				return true;
+			}
+			return false;
+		}
+
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +64,14 @@ public class FriendActivity extends AppCompatActivity {
 			}
 		});
 
-
+		mTextMessage = (TextView) findViewById(R.id.message);
+		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+		Spinner spinner = (Spinner) findViewById(R.id.spinner_nav);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+			R.array.permissions, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
 	}
 
 }
